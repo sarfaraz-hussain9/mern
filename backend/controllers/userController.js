cee789fdb23b2e2db55e5a0ba4e63ef1254eca44
sarfaraz-hussain9 <sarhussai9@gmail.com>
2023-09-23 20:05:04 +0530

    signup controller complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,18 @@
-const signUp=async(req,res,next)=>{
+import User from "../models/userModel.js";
+import errorHandler from "../middlewares/errorHandler.js";
+import bcrypt from "bcryptjs";
 
+const signUp=async(req,res,next)=>{
+const {username,email,password}=req.body;
+const hashedPassword=bcrypt.hashSync(password,10);
+
+const newUser=new User({username,email,password:hashedPassword})
+try {
+    await newUser.save();
+    res.status(200).json({message:"user created successfully"});
+} catch (error) {
+    next(error);
+}
 }
 
 
