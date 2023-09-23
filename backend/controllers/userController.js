c63623928dc8a22bec7367e7292cbe24093db3cb
sarfaraz-hussain9 <sarhussai9@gmail.com>
2023-09-23 21:40:15 +0530

    signin controller completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import errorHandler from "../middlewares/errorHandler.js"
 
 // signup post public
 const signUp=async(req,res,next)=>{
@@ -8,7 +10,7 @@ const signUp=async(req,res,next)=>{
     const newUser=new User({username,email,password:hashedPassword});
     try {
         await newUser.save();
-        res.status(201).json({
+        res.status(200).json({
             message:"user created successfully"
         })
         
@@ -18,9 +20,48 @@ const signUp=async(req,res,next)=>{
 }
 
 
+// signin post public
+const signIn=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler())
+        }
+        const validPassword=bcrypt.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler())
+        }
+        const token=jwt.sign({id:validUser._id},process.env.JWT,{expiresIn:'30d'});
+        res.cookie("Token",token,{httpOnly:true,secure:true,sameSite:"strict", maxAge:30*24*60*60*100 });
+        const{password:hashedPassword,...rest}=validUser._doc;
+        res.status(200).json({rest})
 
+    } catch (error) {
+        next(error)
+    }
+}
+
+// logout post public
+const logOut=async(req,res,next)=>{
+
+}
+
+// profile get private
+const profile=async(req,res,next)=>{
+
+}
+
+// updateprofile put private
+const updateProfile=async(req,res,next)=>{
+
+}
 
 
 export {
-    signUp
+    signUp,
+    signIn,
+    logOut,
+    profile,
+    updateProfile
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controllers/userController.js";
+import { signIn, signUp } from "../controllers/userController.js";
 
 const router=express.Router();
 
 router.post("/signup",signUp);
+router.post("/signin",signIn);
 
 export default router;
